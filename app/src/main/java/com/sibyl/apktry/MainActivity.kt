7eee8d075de52d06548b51f2a3160f93ca9ff711
wanghao <wanghao@szwgmf.com>
2019-03-08 13:34:33 +0800

    プロジェクト作成

---
+++ app/src/main/java/com/sibyl/apktry/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sibyl.apktry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
